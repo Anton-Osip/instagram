c86fc9089c1ce197ec8b3f9aa8f4cc6f79005051
Anton-Osip <osipchika@gmail.com>
2025-02-18 21:59:18 +0300

    Cоздание компонента вручную

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {Test1Component} from './test1/test1.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Test1Component],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
